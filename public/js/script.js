d381c087b0897eb1998e89c129c583cc19d25d09
Keith Kovach <kovachks90@gmail.com>
2018-03-07 19:48:39 -0500

    more game logic fleshed out

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,88 @@
+var guessesLeft = 14;
+var wins = 0;
+var losses = 0;
+
+//Possible letter selections
 var letterString = "abcdefghijklmnopqrstuvwxyz"
 
+//Split letter selections into letter array
 var letterArray = letterString.split("")
 
-var computerGuessArray = ["Obi Wan Kenobi", "Anakin Skywalker", "Luke Skywalker", "Boba Fett"]
+//Array of possible words
+var computerGuessArray = ["obi wan kenobi", "anakin skywalker", "luke skywalker", "boba fett"]
 
-var computerGuessNumber = Math.floor(Math.random() * 4)
-
-var computerGuess = computerGuessArray[computerGuessNumber].split("")
-
-console.log(computerGuess)
-
-var computerGuessHidden = computerGuess
-
+var computerGuessNumber = 0
+var computerGuess = []
+var computerGuessHidden = []
+var guessedLetters = []
 var computerGuessString = ""
+var guessedLetterString = ""
 
-console.log(typeof computerGuess)
 
-for (var i = 0; i < computerGuessHidden.length; i += 1) {
-    
-    if (computerGuessHidden[i] === " ") {
-        computerGuessHidden["test: " + i]
-        computerGuessHidden.splice(i, 1, " ")
-    } else {
-        console.log(i)
-    computerGuessHidden.splice(i, 1, "_")
-
-    }
-    computerGuessString += computerGuessHidden[i]
+function lose() {
+    losses += 1
+    document.getElementById("losses").textContent = "Losses: " + losses;
 }
+
+
+function startGame() {
+    guessesLeft = 14;
+    computerGuessNumber = Math.floor(Math.random() * computerGuessArray.length)
+    computerGuess = computerGuessArray[computerGuessNumber].split("")
+    computerGuessHidden = computerGuessArray[computerGuessNumber].split("")
+
+    //Looping through guessed word and replacing letters with "_" and spaces with nothing
+    for (var i = 0; i < computerGuessHidden.length; i += 1) {
+    
+        if (computerGuessHidden[i] === " ") {
+            computerGuessHidden.splice(i, 1, " ")
+        } 
+        else {
+        computerGuessHidden.splice(i, 1, "_")
+        }
+        
+        computerGuessString += computerGuessHidden[i]
+    }
+    document.getElementById("word").textContent = computerGuessString
+}
+
+startGame()
+
 console.log(typeof computerGuessString)
-console.log(computerGuessString)
 
 document.getElementById("word").textContent = computerGuessString
 
 document.addEventListener("keyup", function(event) {
- console.log(event.key)
- var userGuess = event.key
-    console.log(typeof userGuess)
- if (computerGuess.indexOf(userGuess) > -1) {
-     console.log("correct")
- } else {
-     console.log("incorrect")
- }
+ 
+    var userGuess = event.key.toLowerCase()
+    var guessedIndex = letterArray.indexOf(userGuess)
+    guessedLetterString = ""
+    if (guessedIndex > -1) {
+
+        guessedLetters.push(letterArray[guessedIndex])
+
+        for (var i = 0; i < guessedLetters.length; i += 1) {
+            guessedLetterString += guessedLetters[i] + " "
+        }
+
+        document.getElementById("guessed").textContent = guessedLetterString
+
+        //Removing letter 
+        letterArray.splice(guessedIndex, 1)
+        if (computerGuess.indexOf(userGuess) > -1) {
+            console.log("correct")
+        } 
+        else {
+            guessesLeft -= 1
+            document.getElementById("left").textContent = "Guesses Left: " + guessesLeft
+            if (guessesLeft === 0) {
+                lose()
+                startGame()
+            }
+        }
+    }
+    else {
+        alert("Please select a letter")
+    }
 
 })
